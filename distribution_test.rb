783b324fd1589b01db72705f01eb17cc4c45e68b
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-11-16 13:35:44 -0500

    Some testing up and running. Much more work needed here.

--- distribution_test.rb
+++ distribution_test.rb
@@ -10,15 +10,30 @@ class DistributionTest
   end
 
   def run(count)
+    start_time = Time.now
     puts "Starting test"
     prng = FibonacciRng.new
 
     count.times do
-      @bins[prng.dice(@max)] += 1
+      #@bins[prng.dice(@max)] += 1
+      @bins[rand(@max)] += 1
     end
 
-    puts "Test completed, results:"
-    puts @bins
+    puts "Test completed (#{Time.now - start_time} elapsed)"
+    puts "Raw Bins = #{@bins.inspect}"
+
+    normalized = @bins.collect {|value| value.to_f/count }
+
+    puts "Nrm Bins = #{normalized.inspect}"
+
+    expected = 1/@max.to_f
+    puts "Expected = #{expected}"
+
+    err_sqr = normalized.collect{|value| diff = expected - value; diff*diff}
+    puts "Esq Bins = #{err_sqr.inspect}"
+
+    chi_sq = err_sqr.inject {|sum, value| sum + value}
+    puts "Chi Squared = #{chi_sq}"
   end
 
 end
@@ -26,7 +41,7 @@ end
 bin_limits = (3..1000)
 bin_size = ARGV[0].to_i
 
-count_limits = (1..1000000)
+count_limits = (1..10000000)
 count = ARGV[1].to_i
 
 unless bin_limits === bin_size
