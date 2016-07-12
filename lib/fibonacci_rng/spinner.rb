9b0f9386731fc208fa939425cd4bac6d348c5824
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-07-12 11:33:36 -0400

    Better docs for the stub in spinner.rb

--- lib/fibonacci_rng/spinner.rb
+++ lib/fibonacci_rng/spinner.rb
@@ -12,6 +12,17 @@ class FibonacciRng
 
   #Cycle through the PRNG once.
   #This method is now in ext/fibonacci_rng/fibonacci_rng.c
-  # def do_spin
-  #   code removed.
+  #The original Ruby code follows to document what is done there.
+  #This code is taken from the speed_up_one branch.
+
+# def do_spin
+#   @buffer[-2] = @buffer[0]
+#   @buffer[-1] = p_one = @buffer[1]
+#   (0...@depth).each do |idx|
+#     p_two = @buffer[idx+2]
+#     @buffer[idx] = (p_one + ((p_two >> 1) | (p_two.odd? ? TOP : 0))) & CHOP
+#     p_one = p_two
+#   end
+# end
+
 end
