a672d0e19ab8eea95122c27c087981e2c5be73b5
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-07-13 10:38:38 -0400

    Backed off radical 1.3.0 changes for start of 1.3.1

--- ext/fibonacci_rng/fibonacci_rng.c
+++ ext/fibonacci_rng/fibonacci_rng.c
@@ -20,32 +20,24 @@ static VALUE do_spin(VALUE self)
   // Note: depth + 2 == buffer.length
 
   // @buffer[-2] = @buffer[0]
-  //rb_ary_store(buffer, -2, rb_ary_entry(buffer, 0));
-  RARRAY_ASET(buffer, depth, RARRAY_AREF(buffer, 0));
+  rb_ary_store(buffer, -2, rb_ary_entry(buffer, 0));
 
   // @buffer[-1] = p_one = @buffer[1]
-  //temp = rb_ary_entry(buffer, 1);
-  temp = RARRAY_AREF(buffer, 1);
+  temp = rb_ary_entry(buffer, 1);
   p_one = FIX2INT(temp);
-  //rb_ary_store(buffer, -1, temp);
-  RARRAY_ASET(buffer, depth+1, temp);
+  rb_ary_store(buffer, -1, temp);
 
   // (0...@depth).each do |idx|
   for (idx = 0; idx < depth; idx++)
   {
     // p_two = @buffer[idx+2]
-    //p_two = FIX2INT(rb_ary_entry(buffer, idx+2));
-    p_two = FIX2INT(RARRAY_AREF(buffer, idx+2));
+    p_two = FIX2INT(rb_ary_entry(buffer, idx+2));
 
     // @buffer[idx] = (p_one + ((p_two >> 1) | (p_two.odd? ? TOP : 0))) & CHOP
-    //rb_ary_store(buffer,
-    //             idx,
-    //             INT2FIX((p_one + ((p_two >> 1) | ((p_two & 1) ? TOP : 0))) & CHOP)
-    //            );
-    RARRAY_ASET(buffer,
-                idx,
-                INT2FIX((p_one + ((p_two >> 1) | ((p_two & 1) ? TOP : 0))) & CHOP)
-               );
+    rb_ary_store(buffer,
+                 idx,
+                 INT2FIX((p_one + ((p_two >> 1) | ((p_two & 1) ? TOP : 0))) & CHOP)
+                );
 
     // p_one = p_two
     p_one = p_two;
